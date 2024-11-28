bbad165872f6dfe484b10cb8dda6028cda49ec67
Yvonne Firing <yvonne.firing@noc.ac.uk>
2024-11-28 14:57:40 +0000

    adding docstrings to rainfall_conversion to address issue #3

--- rainfall_conversion.py
+++ rainfall_conversion.py
@@ -1,4 +1,16 @@
+"""
+modules for converting imperial to metric units
+"""
 def inches_to_mm(inches):
+    """
+    Converts rainfall in inches to mm
+
+    Arguments:
+        inches: rainfall in inches
+    
+    Returns: 
+        rainfall in mm
+    """
     mm = inches * 25.4
     return mm
 
